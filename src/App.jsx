8c1e047d83e6c2ea77fd3cc36dcf276e001c29c2
LorenzoFG <lorenzofraterrigo@gmail.com>
2023-06-13 17:20:43 +0200

    i

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from './components/todo'
+import './App.css'
+
+function App() {
+  return (
+  <div className="App">
+    <h1>ToDo List</h1>
+    <Todo/>
+  </div>
+  )
+}
+
+export default App
